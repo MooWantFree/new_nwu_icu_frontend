d2ae00f7e6f7915f0c3d94f92ea18ed8068dc9f9
MooWantFree <i@moo.ac>
2024-07-12 06:33:08 +0800

    update: router url name

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,6 +1,5 @@
 import {createRouter, createWebHistory} from "vue-router";
 import About from "@/views/About.vue";
-import Download from "@/views/Download.vue";
 import Login from "@/views/Login.vue";
 import Reset from "@/views/Reset.vue";
 import Register from "@/views/Register.vue";
@@ -12,18 +11,31 @@ const routes = [
     name: 'home',
     component: () => import('@/views/Home.vue')
   },
-
   {
     path: "/about",
     name: 'about',
     component: About
   },
-  {path: "/download", component: Download},
-  // {path: "/review", component: Review},
-  {path: "/login", component: Login},
-  {path: "/reset", component: Reset},
-  {path: "/register", component: Register},
-  {path: "/review", component: ReviewTimeline},
+  {
+    path: "/login",
+    name: 'login',
+    component: Login
+  },
+  {
+    path: "/reset",
+    name: 'reset',
+    component: Reset
+  },
+  {
+    path: "/register",
+    name: 'register',
+    component: Register
+  },
+  {
+    path: "/review",
+    name: 'review',
+    component: ReviewTimeline
+  },
 ];
 
 const Router = createRouter({
