7b00333230207cc1b7de79cb8a40e350cb576811
lincw <56734434+lincww@users.noreply.github.com>
2024-07-30 12:51:17 +0800

    feat: using loginForm component for /login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,22 @@
 <template>
-
+<div
+  class="login-form"
+>
+  <n-card
+      style="width: 80%;"
+  >
+    <LoginForm />
+  </n-card>
+</div>
 </template>
 
 <script setup lang="ts">
+import LoginForm from '@/components/LoginForm.vue'
 </script>
 
 <style scoped>
+.login-form {
+  display: flex;
+  justify-content: center;
+}
 </style>
