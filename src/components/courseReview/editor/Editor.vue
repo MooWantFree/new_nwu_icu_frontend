3188500a96105ca0301a4f1d01f17f0549d1a948
lincw <56734434+lincww@users.noreply.github.com>
2024-09-16 02:10:08 +0800

    chore: editor

--- src/components/courseReview/editor/Editor.vue
+++ src/components/courseReview/editor/Editor.vue
@@ -2,22 +2,41 @@
   <Milkdown />
 </template>
 <script setup lang="ts">
-import {Milkdown, useEditor} from "@milkdown/vue"
-import {defaultValueCtx, Editor, rootCtx} from "@milkdown/core";
-import {nord} from "@milkdown/theme-nord";
-import {commonmark} from "@milkdown/preset-commonmark";
+import { Milkdown, useEditor } from "@milkdown/vue"
+import { defaultValueCtx, Editor, rootCtx, editorViewOptionsCtx } from "@milkdown/core";
+import { nord } from "@milkdown/theme-nord";
+import { commonmark } from "@milkdown/preset-commonmark";
 
-useEditor((root)=>
-    Editor
-      .make()
-      .config(ctx => {
-        ctx.set(rootCtx, root)
-      })
-      .config(nord)
-      .use(commonmark)
+const props = defineProps<{
+  content: string | null,
+  allowEdit: boolean,
+  withToolbar: boolean,
+}>()
+
+useEditor((root) =>
+  Editor
+    .make()
+    .config(ctx => {
+      ctx.set(rootCtx, root)
+      // If content is provided, set the content to the editor
+      if (props.content) {
+        ctx.set(defaultValueCtx, props.content)
+      }
+      // If allowEdit is true, set the editor to be editable
+      if (props.allowEdit) {
+        ctx.update(editorViewOptionsCtx, (prev) => ({
+          ...prev,
+          editable: () => props.allowEdit,
+        }))
+      }
+      
+      if (props.withToolbar) {
+
+      }
+    })
+    .config(nord)
+    .use(commonmark)
 )
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
