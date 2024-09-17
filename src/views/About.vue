609d776c646c176f17f437bc6fc121555eb79ba0
lincw <56734434+lincww@users.noreply.github.com>
2024-09-17 23:24:14 +0800

    feat[WIP]: editor&viewer with milkdown

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,23 @@
 <template>
-
-    <Markdown/>
-
+  <div v-if="!content">
+    <!-- TODO: Loading...  -->
+  </div>
+  <div v-else>
+    <Editor :content="content" :allow-edit="false" :with-toolbar="false" />
+  </div>
 </template>
 
 <script lang="ts" setup>
+import Editor from "@/components/courseReview/editor/Editor.vue"
+import { ref, onMounted } from "vue"
+const content = ref<string | null>(null)
 
-import Markdown from '@/components/Markdown.vue';
-import {onBeforeMount} from "vue";
+onMounted(async () => {
+  const res = await getAboutContent()
+  content.value = res.data.content
+})
 
-
-
+const getAboutContent = async () => {
+  const resp = await fetch("")
+}
 </script>
