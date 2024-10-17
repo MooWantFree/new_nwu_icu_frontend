80a6364a0d63b6b4dcb25c2c82862ea54adf7437
lincw <56734434+lincww@users.noreply.github.com>
2024-10-18 02:41:49 +0800

    feat: Enhance the review editor's loading state and improve UI feedback during submission processes

--- src/components/courseReview/course/courseReviews/ReviewEditorModal.vue
+++ src/components/courseReview/course/courseReviews/ReviewEditorModal.vue
@@ -1,12 +1,12 @@
 <template>
-  <div v-if="modelValue"
+  <div
     class="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
     <div class="relative w-full max-w-3xl mx-auto my-6 px-4 sm:px-0">
       <div
         class="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none max-h-[90vh]">
         <div class="flex items-start justify-between p-4 sm:p-5 border-b border-solid border-gray-300 rounded-t">
           <h3 class="text-xl sm:text-2xl font-semibold">
-            新建评价
+            {{ initContent.content ? "编辑" : "新建" }}评价
           </h3>
           <button
             class="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none hover:bg-gray-200 transition-colors duration-200 flex items-center justify-center w-8 h-8"
@@ -16,7 +16,7 @@
             </span>
           </button>
         </div>
-        <div class="relative p-4 sm:p-6 flex-auto overflow-y-auto">
+        <div v-if="!loading" class="relative p-4 sm:p-6 flex-auto overflow-y-auto">
           <n-collapse>
             <n-collapse-item title="评价选项" name="review-options">
               <div class="space-y-4">
@@ -53,16 +53,27 @@
           </n-collapse>
           <Editor v-model="content" :allowEdit="true" :withToolbar="true" class="mt-4" />
         </div>
+        <div v-else class="relative p-4 sm:p-6 flex-auto overflow-y-auto">
+          <div class="flex items-center justify-center h-full">
+            <div className="flex flex-col items-center">
+              <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"
+                role="status">
+                <span className="sr-only">加载中...</span>
+              </div>
+              <p className="mt-2 text-center font-medium">加载中</p>
+            </div>
+          </div>
+        </div>
         <div
           class="flex flex-col sm:flex-row items-center justify-end p-4 sm:p-6 border-t border-solid border-gray-300 rounded-b">
           <button
             class="w-full sm:w-auto px-6 py-2 mb-2 sm:mb-0 sm:mr-2 text-sm font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none background-transparent focus:outline-none"
-            type="button" @click="closeModal" :disabled="submitting">
+            type="button" @click="closeModal" :disabled="submitting || loading">
             关闭
           </button>
           <button
             class="w-full sm:w-auto px-6 py-3 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none disabled:bg-blue-300 disabled:cursor-not-allowed flex items-center justify-center"
-            type="button" @click="submitReview" :disabled="submitting">
+            type="button" @click="submitReview" :disabled="submitting || loading">
             <div v-if="submitting" class="mr-2 w-5 h-5 border-t-2 border-white rounded-full animate-spin"></div>
             {{ submitting ? '提交中...' : '提交' }}
           </button>
@@ -70,7 +81,7 @@
       </div>
     </div>
   </div>
-  <div v-if="modelValue" class="fixed inset-0 z-40 bg-black opacity-25"></div>
+  <div class="fixed inset-0 z-40 bg-black opacity-25"></div>
 </template>
 
 <script setup lang="ts">
@@ -79,11 +90,8 @@ import Editor from '@/components/tiptap/editor/Editor.vue'
 import { NewReviewRequest } from "@/types/api/review"
 import { CourseData } from "@/types/courses"
 import { computed, ref } from "vue"
-import { useMessage } from 'naive-ui'
 import { SemesterListResponse } from '@/types/api/course'
 import { api } from '@/lib/requests'
-
-const message = useMessage()
 
 const props = defineProps<{
   courseData: CourseData,
@@ -105,13 +113,15 @@ const difficulty = ref(props.initContent?.difficulty || 3)
 const homework = ref(props.initContent?.homework || 3)
 const grade = ref(props.initContent?.grade || 3)
 const reward = ref(props.initContent?.reward || 3)
-const semesterData = ref<SemesterListResponse['success']|null>(null)
+const semesterData = ref<SemesterListResponse['success'] | null>(null)
 
 const semesterListRequest = api.get<SemesterListResponse>('/api/assessment/semester/')
 
+const loading = ref(true)
 onMounted(async () => {
   const response = await semesterListRequest
   semesterData.value = response.content
+  loading.value = false
 })
 
 const semesterOptions = computed(() => {
@@ -125,12 +135,6 @@ const semesterOptions = computed(() => {
 })
 
 const submitReview = async () => {
-  await semesterListRequest
-  if (!semesterData.value) {
-    message.info('正在加载学期数据，请稍候进行提交...')
-    return
-  }
-
   const reviewData: NewReviewRequest = {
     course: props.courseData.id,
     content: content.value,
