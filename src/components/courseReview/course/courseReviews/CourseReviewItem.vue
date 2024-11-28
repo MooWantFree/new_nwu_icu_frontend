40b921506e37b2d789084fa41e06c0bb5ddb0f3e
lincw <56734434+quul@users.noreply.github.com>
2024-11-28 17:48:58 +0800

    chore: formatter

--- src/components/courseReview/course/courseReviews/CourseReviewItem.vue
+++ src/components/courseReview/course/courseReviews/CourseReviewItem.vue
@@ -3,9 +3,15 @@
     <div class="flex items-center justify-between mb-4">
       <div>
         <div class="flex items-center">
-          <img :src="`/api/download/${review.author.avatar}`" alt="Avatar" class="w-8 h-8 rounded-full mr-2">
+          <img
+            :src="`/api/download/${review.author.avatar}`"
+            alt="Avatar"
+            class="w-8 h-8 rounded-full mr-2"
+          />
           <!-- FIXME: Handle network errors -->
-          <h3 class="text-xl font-bold text-gray-900">{{ review.author.name ?? "匿名用户" }}</h3>
+          <h3 class="text-xl font-bold text-gray-900">
+            {{ review.author.name ?? "匿名用户" }}
+          </h3>
         </div>
         <div class="flex items-center space-x-2 mt-1">
           <div class="flex items-center">
@@ -28,39 +34,82 @@
     </div>
     <div class="flex items-center justify-between text-sm text-gray-500">
       <!-- Dropdown Menu -->
-      <div v-if="isAuthor" class="relative inline-block text-left" ref="dropdownMenu">
-        <button @click="toggleDropdownMenu" id="dropdownButton" type="button"
+      <div
+        v-if="isAuthor"
+        class="relative inline-block text-left"
+        ref="dropdownMenu"
+      >
+        <button
+          @click="toggleDropdownMenu"
+          id="dropdownButton"
+          type="button"
           class="inline-flex items-center p-2 text-sm font-medium text-gray-700 bg-white rounded-md hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          aria-expanded="true" aria-haspopup="true">
+          aria-expanded="true"
+          aria-haspopup="true"
+        >
           <span class="sr-only">打开下拉菜单</span>
-          <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-            aria-hidden="true">
-            <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+          <svg
+            class="w-5 h-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
+            />
           </svg>
         </button>
 
-        <div v-if="showDropdownMenu" id="dropdownMenu"
+        <div
+          v-if="showDropdownMenu"
+          id="dropdownMenu"
           class="z-50 origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
-          role="menu" aria-orientation="vertical" aria-labelledby="dropdownButton" tabindex="-1">
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="dropdownButton"
+          tabindex="-1"
+        >
           <div class="py-1" role="none">
-            <button @click="handleEdit"
+            <button
+              @click="handleEdit"
               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
-              role="menuitem" tabindex="-1" id="editButton">
-              <svg class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              role="menuitem"
+              tabindex="-1"
+              id="editButton"
+            >
+              <svg
+                class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
                 <path
-                  d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                  d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+                />
               </svg>
               编辑评价
             </button>
-            <button @click="handleDelete"
+            <button
+              @click="handleDelete"
               class="group flex items-center px-4 py-2 text-sm text-red-700 hover:bg-red-100 hover:text-red-900 w-full text-left"
-              role="menuitem" tabindex="-1" id="deleteButton">
-              <svg class="mr-3 h-5 w-5 text-red-400 group-hover:text-red-500" xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd"
+              role="menuitem"
+              tabindex="-1"
+              id="deleteButton"
+            >
+              <svg
+                class="mr-3 h-5 w-5 text-red-400 group-hover:text-red-500"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
                   d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clip-rule="evenodd" />
+                  clip-rule="evenodd"
+                />
               </svg>
               删除评价
             </button>
@@ -70,29 +119,47 @@
       <div class="flex items-center space-x-2">
         <Time :time="new Date(review.created_time)" />
         <div v-if="review.edited">
-          <span>(最后修改于: <n-time :time="new Date(review.modified_time)" />)</span>
+          <span
+            >(最后修改于:
+            <n-time :time="new Date(review.modified_time)" />)</span
+          >
         </div>
       </div>
     </div>
     <div class="mt-4">
-      <div class="flex justify-between items-center mb-2" v-if="review.reply && review.reply.length > 0">
+      <div
+        class="flex justify-between items-center mb-2"
+        v-if="review.reply && review.reply.length > 0"
+      >
         <h3 class="font-semibold text-gray-900 text-lg">评论</h3>
         <!-- TODO: Change color -->
         <div>
           <span>排序：</span>
-          <n-button text @click="toggleReplyOrder" class="text-blue-600 hover:text-blue-800">
-            {{ reverseReplies ? '最新回复' : '最早回复' }}
+          <n-button
+            text
+            @click="toggleReplyOrder"
+            class="text-blue-600 hover:text-blue-800"
+          >
+            {{ reverseReplies ? "最新回复" : "最早回复" }}
           </n-button>
         </div>
       </div>
       <div class="space-y-2">
-        <div v-for="(reply, index) in (reverseReplies ? [...review.reply].reverse() : review.reply)"
-          :key="reply.created_time" class="bg-gray-50 p-3 rounded-md flex justify-between items-start relative group">
+        <div
+          v-for="(reply, index) in reverseReplies
+            ? [...review.reply].reverse()
+            : review.reply"
+          :key="reply.created_time"
+          class="bg-gray-50 p-3 rounded-md flex justify-between items-start relative group"
+        >
           <div>
             <p class="text-sm text-gray-700">
               <span>
-                <router-link v-if="reply.created_by.id > 0" :to="`/user/${reply.created_by.id}`"
-                  class="text-blue-600 hover:underline">
+                <router-link
+                  v-if="reply.created_by.id > 0"
+                  :to="`/user/${reply.created_by.id}`"
+                  class="text-blue-600 hover:underline"
+                >
                   {{ reply.created_by.name }}
                 </router-link>
                 <span v-else>
@@ -101,27 +168,52 @@
               </span>
               <span v-if="reply.parent > 0">
                 回复了
-                <router-link v-if="review.reply.find(it => it.id === reply.parent)?.created_by.id > 0"
-                  :to="`/user/${review.reply.find(it => it.id === reply.parent)?.created_by.id}`"
-                  class="text-blue-600 hover:underline">
-                  {{ review.reply.find(it => it.id === reply.parent)?.created_by.name }}
+                <router-link
+                  v-if="
+                    review.reply.find((it) => it.id === reply.parent)
+                      ?.created_by.id > 0
+                  "
+                  :to="`/user/${
+                    review.reply.find((it) => it.id === reply.parent)
+                      ?.created_by.id
+                  }`"
+                  class="text-blue-600 hover:underline"
+                >
+                  {{
+                    review.reply.find((it) => it.id === reply.parent)
+                      ?.created_by.name
+                  }}
                 </router-link>
                 (
-                <a class="text-blue-600 hover:underline" @click="handleJmpClick(review.reply.findIndex(it => it.id === reply.parent))">
-                  #{{ reverseReplies ? review.reply.findIndex(it => it.id === reply.parent) + 1 : review.reply.length -
-                  review.reply.findIndex(it => it.id === reply.parent) }}
+                <a
+                  class="text-blue-600 hover:underline"
+                  @click="
+                    handleJmpClick(
+                      review.reply.findIndex((it) => it.id === reply.parent)
+                    )
+                  "
+                >
+                  #{{
+                    reverseReplies
+                      ? review.reply.findIndex((it) => it.id === reply.parent) +
+                        1
+                      : review.reply.length -
+                        review.reply.findIndex((it) => it.id === reply.parent)
+                  }}
                 </a>
                 )
               </span>
-              <span>
-                : {{ reply.content }}
-              </span>
+              <span> : {{ reply.content }} </span>
             </p>
             <p class="text-xs text-gray-500 mt-1">
               <Time type="relative" :time="new Date(reply.created_time)" />
               &nbsp;
-              <n-button v-if="isAuthor" text @click="() => handleDeleteReply(reply.id)"
-                class="text-red-600 hover:text-red-800">
+              <n-button
+                v-if="isAuthor"
+                text
+                @click="() => handleDeleteReply(reply.id)"
+                class="text-red-600 hover:text-red-800"
+              >
                 <!-- FIXME: Why hover become green? -->
                 删除
               </n-button>
@@ -131,8 +223,11 @@
             <span class="text-sm text-gray-500 ml-2 group-hover:hidden">
               #{{ reverseReplies ? index + 1 : review.reply.length - index }}
             </span>
-            <n-button text @click="() => toggleReply(reply.id)"
-              class="absolute top-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-blue-600 hover:text-blue-800">
+            <n-button
+              text
+              @click="() => toggleReply(reply.id)"
+              class="absolute top-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-blue-600 hover:text-blue-800"
+            >
               回复
             </n-button>
           </div>
@@ -143,171 +238,197 @@
       </div>
     </div>
     <div class="flex justify-end mt-2 mx-2">
-      <n-button v-if="isLoggedIn" text @click="() => toggleReply()" class="text-blue-600 hover:text-blue-800">
-        {{ showReply ? '取消回复' : '回复' }}
+      <n-button
+        v-if="isLoggedIn"
+        text
+        @click="() => toggleReply()"
+        class="text-blue-600 hover:text-blue-800"
+      >
+        {{ showReply ? "取消回复" : "回复" }}
       </n-button>
-      <span v-else>
-        登录以后才能回复
-      </span>
+      <span v-else> 登录以后才能回复 </span>
     </div>
-    <CourseReviewItemReply ref="replyTextArea" v-if="isLoggedIn && showReply" :review="review" :reply-to="replyTarget"
-      @close="toggleReply" class="mt-4" @replySubmitted="onReplySubmitted" />
+    <CourseReviewItemReply
+      ref="replyTextArea"
+      v-if="isLoggedIn && showReply"
+      :review="review"
+      :reply-to="replyTarget"
+      @close="toggleReply"
+      class="mt-4"
+      @replySubmitted="onReplySubmitted"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { Review } from '@/types/courses'
-import { onMounted, onUnmounted, ref, computed, watch, useTemplateRef, nextTick } from 'vue'
-import { useRoute } from 'vue-router'
-import { NButton, useMessage, useDialog } from 'naive-ui'
-import { useUser } from '@/lib/useUser'
-import { api } from '@/lib/requests'
-import Time from '@/components/shortcuts/Time.vue'
-import Viewer from '@/components/tiptap/viewer/Viewer.vue'
-import CourseReviewItemReply from './CourseReviewItemReply.vue'
-import type { ReplyDeleteResponse } from '@/types/api/course'
+import { Review } from "@/types/courses";
+import {
+  onMounted,
+  onUnmounted,
+  ref,
+  computed,
+  watch,
+  useTemplateRef,
+  nextTick,
+} from "vue";
+import { useRoute } from "vue-router";
+import { NButton, useMessage, useDialog } from "naive-ui";
+import { useUser } from "@/lib/useUser";
+import { api } from "@/lib/requests";
+import Time from "@/components/shortcuts/Time.vue";
+import Viewer from "@/components/tiptap/viewer/Viewer.vue";
+import CourseReviewItemReply from "./CourseReviewItemReply.vue";
+import type { ReplyDeleteResponse } from "@/types/api/course";
 
 const props = defineProps<{
-  review: Review,
-}>()
+  review: Review;
+}>();
 
 const emit = defineEmits<{
-  (e: 'reviewDeleted', id: number): void,
-  (e: 'reviewEdit'): void,
-}>()
+  (e: "reviewDeleted", id: number): void;
+  (e: "reviewEdit"): void;
+}>();
 
-const message = useMessage()
-const dialog = useDialog()
-const route = useRoute()
-const courseReviewItem = useTemplateRef('courseReviewItem')
+const message = useMessage();
+const dialog = useDialog();
+const route = useRoute();
+const courseReviewItem = useTemplateRef("courseReviewItem");
 
 // Scroll if has hash
 onMounted(async () => {
-  const hash = route.hash
+  const hash = route.hash;
   if (hash && hash.includes(`review-${props.review.id}`)) {
-    await nextTick()
+    await nextTick();
     if (courseReviewItem.value) {
-      courseReviewItem.value.scrollIntoView({ behavior: 'smooth' })
+      courseReviewItem.value.scrollIntoView({ behavior: "smooth" });
     }
   }
-})
+});
 
 // Display the reply box or not
-const showReply = ref(false)
+const showReply = ref(false);
 const isAuthor = computed(() => {
-  return props.review.author.id === userInfo.value?.id
-})
+  return props.review.author.id === userInfo.value?.id;
+});
 
-const replyTarget = ref(0)
-const replyTextArea = useTemplateRef('replyTextArea')
+const replyTarget = ref(0);
+const replyTextArea = useTemplateRef("replyTextArea");
 const toggleReply = (replyTo: number = 0) => {
-  replyTarget.value = replyTo
+  replyTarget.value = replyTo;
   if (replyTo !== 0) {
     if (!showReply.value) {
-      showReply.value = !showReply.value
+      showReply.value = !showReply.value;
     }
   } else {
-    showReply.value = !showReply.value
+    showReply.value = !showReply.value;
   }
   if (showReply.value) {
     nextTick(() => {
-      replyTextArea.value?.focus()
-    })
+      replyTextArea.value?.focus();
+    });
   }
-}
+};
 
-const reverseReplies = ref(false)
+const reverseReplies = ref(false);
 
 const toggleReplyOrder = () => {
-  reverseReplies.value = !reverseReplies.value
-}
+  reverseReplies.value = !reverseReplies.value;
+};
 
-const { userInfo, isLoggedIn } = useUser()
+const { userInfo, isLoggedIn } = useUser();
 const onReplySubmitted = (content: string, parent: number) => {
-  toggleReply()
+  toggleReply();
   // Push the new reply to the review's replies array
   props.review.reply.unshift({
     id: props.review.reply.length + 1,
     parent,
     content,
-    created_time: new Date().toISOString(), // Changed 'created_at' to 'created_time'
+    created_time: new Date().toISOString(),
     created_by: {
       id: userInfo.value.id,
       name: userInfo.value.nickname ?? userInfo.value.username,
-      avatar: userInfo.value.avatar
+      avatar: userInfo.value.avatar,
     },
     like: {
       like: 0,
-      dislike: 0
-    }
-  })
-}
+      dislike: 0,
+      user_option: 0,
+    },
+  });
+};
 
 // Dropdown menu
-const dropdownMenuRef = useTemplateRef('dropdownMenu')
-const showDropdownMenu = ref(false)
+const dropdownMenuRef = useTemplateRef("dropdownMenu");
+const showDropdownMenu = ref(false);
 const toggleDropdownMenu = () => {
-  handleDropdownMenuOpened()
-  showDropdownMenu.value = !showDropdownMenu.value
-}
+  handleDropdownMenuOpened();
+  showDropdownMenu.value = !showDropdownMenu.value;
+};
 const handleClickEvent = (e: MouseEvent) => {
   if (dropdownMenuRef && !dropdownMenuRef.value.contains(e.target as Node)) {
-    showDropdownMenu.value = false
+    showDropdownMenu.value = false;
   }
-}
+};
 const handleDropdownMenuClosed = () => {
-  document.removeEventListener('click', handleClickEvent)
-}
+  document.removeEventListener("click", handleClickEvent);
+};
 const handleDropdownMenuOpened = () => {
-  document.addEventListener('click', handleClickEvent)
-}
+  document.addEventListener("click", handleClickEvent);
+};
 onUnmounted(() => {
-  document.removeEventListener('click', handleClickEvent)
-})
+  document.removeEventListener("click", handleClickEvent);
+});
 watch(showDropdownMenu, (newValue) => {
   if (!newValue) {
-    handleDropdownMenuClosed()
+    handleDropdownMenuClosed();
   }
-})
+});
 
 const handleEdit = () => {
-  emit('reviewEdit')
-  showDropdownMenu.value = false
-}
+  emit("reviewEdit");
+  showDropdownMenu.value = false;
+};
 
 const handleDelete = async () => {
   // FIXME: Still not able to work
   dialog.warning({
-    title: '确认删除',
-    content: '你确定你想要删除这条评价吗？删除以后不可恢复！',
-    positiveText: '确定',
-    negativeText: '取消',
+    title: "确认删除",
+    content: "你确定你想要删除这条评价吗？删除以后不可恢复！",
+    positiveText: "确定",
+    negativeText: "取消",
     onPositiveClick: async () => {
       try {
-        const response = await api.delete<ReplyDeleteResponse>(`/api/assessment/reply/`)
+        const response = await api.delete<ReplyDeleteResponse>(
+          `/api/assessment/reply/`
+        );
         if (response.status === 200) {
-          message.success('评价已成功删除')
+          message.success("评价已成功删除");
           // TODO: Handle the reply delete item
         } else {
-          throw new Error(response.errors.reduce((acc, cur) => acc + cur.field + ': ' + cur.err_msg + '\n', ''))
+          throw new Error(
+            response.errors.reduce(
+              (acc, cur) => acc + cur.field + ": " + cur.err_msg + "\n",
+              ""
+            )
+          );
         }
       } catch (error) {
-        console.error('Error deleting reply:', error)
-        message.error('删除评价失败，请稍后重试\n' + error)
+        console.error("Error deleting reply:", error);
+        message.error("删除评价失败，请稍后重试\n" + error);
       }
-    }
-  })
-}
+    },
+  });
+};
 
 const handleDeleteReply = async (repyId: number) => {
   if (confirm(`你确定要删掉这条回复吗？！`)) {
-    
+    // TODO
   }
-}
+};
 
 const handleJmpClick = (targetReplyId: number) => {
   // TODO
-}
+};
 </script>
 
 <style scoped></style>
