cb14e794d85b5359644856a1905c6784af69eee4
lincw <56734434+quul@users.noreply.github.com>
2024-12-26 23:29:21 +0800

    fix: search teacher component jump

--- src/components/search/results/SearchResultTeacher.vue
+++ src/components/search/results/SearchResultTeacher.vue
@@ -4,16 +4,27 @@
     class="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow"
   >
     <!-- Teacher information section with avatar -->
-    <div class="flex items-center mb-4">
-      <img
-        :src="`/api/file/${teacher.avatar_uuid}`"
-        alt="Teacher Avatar"
-        class="w-16 h-16 rounded-full mr-4 object-cover border-2 border-gray-200"
-      />
-      <div>
-        <!-- Teacher name -->
-        <h3 class="text-lg font-semibold text-blue-600">{{ teacher.name }}</h3>
-        <!-- School information -->
+    <div class="flex items-center mb-4 cursor-pointer" @click="handleTeacherClick">
+      <n-avatar round :src="`/api/download/${teacher.avatar_uuid}`">
+        <template #fallback>
+          <div
+            :class="[
+              'w-full h-full flex justify-center items-center text-white text-2xl font-semibold',
+              [
+                'bg-indigo-500',
+                'bg-teal-600',
+                'bg-orange-600',
+                'bg-pink-600',
+                'bg-cyan-600',
+              ][teacher.name.charCodeAt(0) % 5],
+            ]"
+          >
+            {{ teacher.name.charAt(0).toUpperCase() }}
+          </div>
+        </template>
+      </n-avatar>
+      <div class="ml-4">
+        <h3 class="text-lg font-semibold text-blue-600 hover:underline">{{ teacher.name }}</h3>
         <p class="text-sm text-gray-600">{{ teacher.school }}</p>
       </div>
     </div>
@@ -28,9 +39,20 @@
 <script setup lang="ts">
 // Import the TeacherSearchResult type for type checking
 import { TeacherSearchResult } from '@/types/api/search/search'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 // Define props with TypeScript type
-defineProps<{
+const { teacher } = defineProps<{
   teacher: TeacherSearchResult
 }>()
+const emit = defineEmits<{
+  (e: 'close'): void
+}>()
+
+const handleTeacherClick = async () => {
+  router.push(`/review/teacher/${teacher.id}`)
+  emit('close')
+}
 </script>
