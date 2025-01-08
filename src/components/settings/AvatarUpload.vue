d7dd65cd51d1fc0302092e79c221188516af45f5
lincw <56734434+quul@users.noreply.github.com>
2025-01-09 00:44:07 +0800

    feat: better progress UI for avatar upload

--- src/components/settings/AvatarUpload.vue
+++ src/components/settings/AvatarUpload.vue
@@ -76,16 +76,24 @@
             </button>
           </div>
         </div>
-        <div
-          v-if="progress > 0"
-          class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700"
-        >
-          <div
-            class="bg-blue-600 h-2.5 rounded-full"
-            :style="{ width: `${progress}%` }"
-          ></div>
-          <div class="text-center text-sm text-gray-600 mt-1">
-            {{ progress }}%
+        <div v-if="progress > 0" class="relative pt-1">
+          <div class="flex mb-2 items-center justify-between">
+            <div>
+              <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blue-600 bg-blue-200">
+                上传进度
+              </span>
+            </div>
+            <div class="text-right">
+              <span class="text-xs font-semibold inline-block text-blue-600">
+                {{ progress }}%
+              </span>
+            </div>
+          </div>
+          <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
+            <div
+              :style="{ width: `${progress}%` }"
+              class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500 transition-all duration-300 ease-in-out"
+            ></div>
           </div>
         </div>
         <hr class="my-4" />
