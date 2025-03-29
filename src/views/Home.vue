d4b332fb9f2a88bd7fe698929ac0e5b517e5a8a6
MooWantFree <i@moo.ac>
2025-03-29 11:07:34 +0800

    fix: change home layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,60 @@
 <template>
   <div class="min-h-screen">
     <main class="container mx-auto py-12 px-6">
-      <div
-        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 items-start"
-      >
-        <div
-          v-for="section in sections"
-          :key="section.title"
-          class="bg-white/50 backdrop-blur-md rounded-lg shadow-md p-6 flex flex-col"
-        >
-          <div class="flex items-center gap-4 mb-4">
-            <div class="bg-primary rounded-full p-2">
-              <BookIcon class="w-6 h-6 text-primary-foreground" />
-            </div>
-            <h2 class="text-lg font-semibold">{{ section.title }}</h2>
+      <div class="flex flex-col md:flex-row">
+        <div class="flex-grow">
+          <div>
+            <span :class="['text-3xl font-bold pb-2 text-gray-600 inline-block']">Welcome to&nbsp;</span><span
+            :class="['text-3xl font-bold pb-2 text-gray-600 inline-block', { 'animate__animated animate__rubberBand': showAnimation }]"
+          > NWU.ICU</span>
           </div>
-          <div class="grid grid-cols-2 gap-4 auto-rows-fr">
-            <a
-              v-for="item in section.items"
-              :key="item.label"
-              :href="item.url"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="bg-white/50 backdrop-blur-md rounded-lg shadow-md p-4 flex flex-col items-center justify-center gap-2 hover:bg-slate-500 hover:text-slate-50 transition-colors"
-            >
-              <component :is="item.icon" class="w-8 h-8" />
-              <span>{{ item.label }}</span>
-            </a>
+          <div v-for="section in sections" :key="section.title"
+               class="bg-white/50 backdrop-blur-md rounded-lg shadow-md p-6 flex flex-col">
+            <div class="flex items-center gap-4 mb-4">
+              <div class="bg-primary rounded-full p-2">
+                <BookIcon class="w-6 h-6 text-primary-foreground" />
+              </div>
+              <h2 class="text-lg font-semibold">{{ section.title }}</h2>
+            </div>
+            <div class="grid grid-cols-2 gap-4 auto-rows-fr">
+              <a v-for="item in section.items" :key="item.label" :href="item.url" target="_blank"
+                 rel="noopener noreferrer"
+                 class="bg-white/50 backdrop-blur-md rounded-lg shadow-md p-4 flex flex-col items-center justify-center gap-2 hover:bg-slate-500 hover:text-slate-50 transition-colors">
+                <component :is="item.icon" class="w-8 h-8" />
+                <span>{{ item.label }}</span>
+              </a>
+            </div>
           </div>
         </div>
+
+        <div class="flex-col">
+          <router-link :to="'/review/timeline'" class="block">
+            <h1
+              class="text-3xl underline font-bold text-gray-900 px-4 pb-4 sm:px-6 lg:px-8 hover:text-primary transition-colors mt-8 md:mt-0">
+              最近评价</h1>
+          </router-link>
+
+          <ReviewTimeline :showHeader="false" :pageSize="3" />
+        </div>
       </div>
+
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Bike, BookOpenText, Library, Mail, Printer, Waypoints } from 'lucide-vue-next'
+import { Bike, BookOpenText, Bus, Laptop, Library, Mail, Phone, Printer, Waypoints } from 'lucide-vue-next'
+import ReviewTimeline from '@/views/courseReview/ReviewTimeline.vue'
+import { onMounted, ref } from 'vue'
+
+const showAnimation = ref(false)
+
+onMounted(() => {
+  setTimeout(() => {
+    showAnimation.value = true
+  }, 200)
+})
+
 const sections = [
   {
     title: '西大服务',
@@ -50,6 +69,21 @@ const sections = [
         label: '图书馆馆藏检索',
         icon: Library,
         url: 'https://agentdockingopac.featurelib.libsou.com/showhome/search/showSearch?schoolId=25',
+      },
+      {
+        label: '班车时刻表',
+        icon: Bus,
+        url: 'https://hqjt.nwu.edu.cn/fwdt/bcsk.htm',
+      },
+      {
+        label: '西大电话',
+        icon: Phone,
+        url: 'https://www.nwu.edu.cn/ggzy/xddh.htm',
+      },
+      {
+        label: '正版软件',
+        icon: Laptop,
+        url: 'https://zhengban.nwu.edu.cn/',
       },
     ],
   },
