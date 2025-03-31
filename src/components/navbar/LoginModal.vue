61d07f3d98c6d122aa369398b817de3862f0f13d
MooWantFree <i@moo.ac>
2025-03-31 16:48:00 +0800

    fix: make loginForm in center of screen

--- src/components/navbar/LoginModal.vue
+++ src/components/navbar/LoginModal.vue
@@ -6,7 +6,7 @@
     role="dialog"
     aria-modal="true"
   >
-    <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+    <div class="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center">
       <!-- Background overlay -->
       <div
         class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
@@ -16,18 +16,14 @@
 
       <!-- Modal panel -->
       <div
-        class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+        class="relative inline-block w-full max-w-md p-6 my-8 bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all"
       >
-        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-          <div class="sm:flex sm:items-start">
-            <div class="mt-3 text-center sm:mt-0 sm:text-left w-full">
-              <div class="mt-4">
-                <LoginForm 
-                  @login-success="handleLoginSuccess" 
-                  @close-modal="$emit('close')" 
-                />
-              </div>
-            </div>
+        <div class="w-full">
+          <div class="w-full">
+            <LoginForm 
+              @login-success="handleLoginSuccess" 
+              @close-modal="$emit('close')" 
+            />
           </div>
         </div>
       </div>
